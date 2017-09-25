308c46eb4b8fa83a56ed2e9381513778640c820e
Varjitt Jeeva <varjitt.jeeva@gmail.com>
2017-09-25 12:07:33 -0700

    Updated postgres driver and README

--- lib/engines/postgresql.rb
+++ lib/engines/postgresql.rb
@@ -7,13 +7,21 @@ module Uphold
         @docker_image ||= 'postgres'
         @docker_tag ||= '9.5.0'
         @docker_env ||= ["POSTGRES_USER=#{@database}", "POSTGRES_DB=#{@database}"]
+        @file_type ||= 'sql'
         @port ||= 5432
+        @username ||= 'postgres'
         @sql_file = params[:sql_file] ||  'PostgreSQL.sql'
       end
 
       def load_backup(path)
         Dir.chdir(path) do
-          run_command("psql --no-password --set ON_ERROR_STOP=on --username=#{@database} --host=#{container_ip_address} --port=#{@port} --dbname=#{@database} < #{@sql_file}")
+          if @file_type.contains("sql")
+            run_command("psql --no-password --set ON_ERROR_STOP=on --username=#{@username} --host=#{container_ip_address} --port=#{@port} --dbname=#{@database} < #{@sql_file}")
+          elsif @file_type.contains("dump")
+             run_command("pg_restore --no-password --set ON_ERROR_STOP=on --username=#{@username} --host=#{container_ip_address} --port=#{@port} --dbname=#{@database} < #{@sql_file}")i
+          else
+            raise 'File Type parameter in Postgresql Driver is invalid.'
+          end
         end
       end
     end
